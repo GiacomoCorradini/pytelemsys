79e950667910a418cb60e112326d9729e5729c1a
Giacomo Corradini <corra981@gmail.com>
2025-04-14 22:50:19 +0200

    Update pytelem test

--- tests/test_pytelem.py
+++ tests/test_pytelem.py
@@ -1,16 +1,16 @@
 # %% Setup
 import matplotlib.pyplot as plt
 import os
-import sys
 
 from pytelemsys.pytelem import TelemetryData
-from pytelemsys.converter import mlt_gp2_converter
+from pytelemsys.converter import mlt_gp2_converter, gp2_converter
+from pytelemsys.utils.conversion import GPS2XYZ_ENU
 
 # Get root path
 root_path = os.path.abspath(os.path.join(__file__, "../"))
 
 # %% Initialize the TrackData object
-track_data = TelemetryData(
+ocp_telem_data = TelemetryData(
     os.path.join(root_path, "telemetry/GP2_OCP.txt"),
     separator="\t",
     comment="#",
@@ -18,12 +18,34 @@ track_data = TelemetryData(
     fun_conversion=mlt_gp2_converter,
 )
 
+gp2_telem_data = TelemetryData(
+    os.path.join(root_path, "telemetry/GP2_TELEM_Q.csv"),
+    separator=";",
+    comment="#",
+    decimal=",",
+    fun_conversion=gp2_converter,
+)
+
+# %% Convert the ENUs to Cartesian coordinates
+gp2_telem_data.data["x"], gp2_telem_data.data["y"], gp2_telem_data.data["z"] = (
+    GPS2XYZ_ENU(
+        gp2_telem_data.data["lon"],
+        gp2_telem_data.data["lat"],
+        gp2_telem_data.data["z"],
+        origin=(
+            gp2_telem_data.data["lon"].iloc[0],
+            gp2_telem_data.data["lat"].iloc[0],
+            gp2_telem_data.data["z"].iloc[0],
+        ),
+    )
+)
+
 # %% Resample the data
 sampling_freq = 100
-track_data_resample = track_data.resample(ref_column="time", freq=sampling_freq)
+ocp_telem_data_resample = ocp_telem_data.resample(ref_column="time", freq=sampling_freq)
 
 # Validate that all time differences are equal to 1/freq
-time_diff = track_data_resample["time"].diff().dropna()
+time_diff = ocp_telem_data_resample["time"].diff().dropna()
 expected_diff = 1 / sampling_freq
 if not all(abs(time_diff - expected_diff) < 1e-6):
     raise ValueError(
@@ -32,22 +54,52 @@ if not all(abs(time_diff - expected_diff) < 1e-6):
 
 # %% Plot the data resampled (check only the accelerations)
 fig, ax = plt.subplots(3, 1, figsize=(10, 6))
-ax[0].plot(track_data.data["time"], track_data.data["ax"], label="Raw")
+ax[0].plot(ocp_telem_data.data["time"], ocp_telem_data.data["ax"], label="Raw")
 ax[0].plot(
-    track_data_resample["time"], track_data_resample["ax"], "--", label="Resampled"
-)
-
-ax[1].plot(track_data.data["time"], track_data.data["ay"], label="Raw")
-ax[1].plot(
-    track_data_resample["time"], track_data_resample["ay"], "--", label="Resampled"
-)
-
-ax[2].plot(track_data.data["time"], track_data.data["yaw_rate"], label="Raw")
-ax[2].plot(
-    track_data_resample["time"],
-    track_data_resample["yaw_rate"],
+    ocp_telem_data_resample["time"],
+    ocp_telem_data_resample["ax"],
     "--",
     label="Resampled",
 )
 
-# %%
+
+ax[1].plot(ocp_telem_data.data["time"], ocp_telem_data.data["ay"], label="Raw")
+ax[1].plot(
+    ocp_telem_data_resample["time"],
+    ocp_telem_data_resample["ay"],
+    "--",
+    label="Resampled",
+)
+
+ax[2].plot(ocp_telem_data.data["time"], ocp_telem_data.data["yaw_rate"], label="Raw")
+ax[2].plot(
+    ocp_telem_data_resample["time"],
+    ocp_telem_data_resample["yaw_rate"],
+    "--",
+    label="Resampled",
+)
+
+fig.tight_layout(pad=0)
+
+# %% Plot trajectory
+fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+ax.plot(
+    gp2_telem_data.data["x"],
+    gp2_telem_data.data["y"],
+    label="GP2",
+    color="blue",
+    alpha=0.5,
+)
+ax.plot(
+    ocp_telem_data.data["x"],
+    ocp_telem_data.data["y"],
+    label="OCP",
+    color="red",
+    alpha=0.5,
+)
+ax.set_xlabel("X [m]")
+ax.set_ylabel("Y [m]")
+ax.set_title("Trajectory")
+ax.legend()
+
+plt.show()
